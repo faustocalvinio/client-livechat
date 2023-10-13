2ffa83387b2a51bd12b0d9d593c18ef6abd87d9d
Fausto Calviño <faustocalvino@outlook.com>
2023-10-13 16:56:47 +0200

    feat: working on UI and messages lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import io from "socket.io-client";
 import { useEffect, useState } from "react";
+import { useRef } from "react";
 
-const socket = io.connect("http://localhost:3001");
+const backURL=import.meta.env.VITE_BACK_URL
+const socket = io.connect(backURL);
+
 
 export default function App ()  {  
-  const [room, setRoom] = useState(1);
-  const [message, setMessage] = useState("");
-  const [messageReceived, setMessageReceived] = useState("");
-  const [messages, setMessages] = useState([]);
-
   const [theme] = useState(() => {
     if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
       return "dark";
@@ -31,54 +29,83 @@ export default function App ()  {
     }
   };
 
-  const sendMessage = () => {
-    socket.emit("send_message", { message, room });
-  };
 
-  useEffect(() => {
-    socket.on("receive_message", (data) => {
-      setMessageReceived(data.message);
-      const arr = [...messages]
-      console.log(arr);
-      arr.push(data.message)
-      console.log(arr);
-      setMessages(arr);
+  const textInput = useRef();
+  const usernameTextInput = useRef();
+  const [room] = useState(1);
+  const [username, setUsername] = useState('anon')
+  const [allMessagesFromBackend, setAllMessagesFromBackend] = useState([
+   
+  ])
+
+
+  const onSubmitUsername=(e)=>{
+    e.preventDefault();
+    if (usernameTextInput.current.value === "") return;
+    setUsername(usernameTextInput.current.value);
+    usernameTextInput.current.classList.add('hidden')
+  }
+
+  // const onSubmitMessage=(e)=>{
+  //   e.preventDefault();
+  //   if (textInput.current.value === "") return;
     
-      // setMessages([...messages, data.message]);
-      // console.log(messages);
-    });
-  }, [socket]);
+  //   socket.emit("send_message",textInput.current.value)
+  //   console.log(textInput.current.value);
+  //   setAllMessagesState([...allMessagesState,textInput.current.value]);
+  //   textInput.current.value = '';
+  // }
+  
+  const onSubmitMessage=(e)=>{
+    e.preventDefault();
+    if (textInput.current.value === "") return;
+    socket.emit("send_message",{message:textInput.current.value,room,user:username})
+    setAllMessagesFromBackend([...allMessagesFromBackend,{message:textInput.current.value,room,user:username}]);
+    textInput.current.value = '';
+  }
+  
 
-  useEffect(() => {
-    joinRoom(); 
-  },[messages])
+
+  
+
+  useEffect(() => {  
+    joinRoom();      
+    socket.on("new_message", (data) => {
+      console.log(data);
+      setAllMessagesFromBackend(data)
+    })      
+  },[])
 
   return (
-    <div className="App dark:text-white">
-      <h1>Room Number: {room} </h1>
-      <input
-        placeholder="Room Number..."
-        onChange={(event) => {
-          setRoom(event.target.value);
-        }}
-      />
-      <button onClick={joinRoom}> Join Room</button>
-      <input
-        placeholder="Message..."
-        onChange={(event) => {
-          setMessage(event.target.value);
-        }}
-      />
-      <button onClick={sendMessage}> Send Message</button>
-      <h1> Message:</h1>
-      {messageReceived}
+    <div className="dark:bg-black dark:text-white max-w-[1100px] mx-auto  flex flex-col justify-between min-h-[90vh] max-[1100px]:w-[90%] max-[1100px]:mx-auto">
+      <div className="flex flex-col gap-4">
+        <form action="" onSubmit={onSubmitUsername}>
+          <input className="mt-4 bg-gray-200 dark:bg-gray-700 rounded-full py-2 px-6  w-full" 
+          ref={usernameTextInput}
+          type="text" name="" id="" placeholder="username" />
+        </form>
+        
+        <h1 className="text-xl">Welcome {username} !!! Room Number: {room} </h1> 
+        
+        <h4 className="text-xl">{allMessagesFromBackend.length!==0 ? 'Todos los mensajes' : 'No hay mensajes para mostrar' }</h4>
         <ul>
-            {
-              messages.map((message, index) => {
-                <li key={index} >{message}</li>
-              })
-            }
-        </ul>
+          {
+            allMessagesFromBackend.map((message)=><li className="text-xl" key={crypto.randomUUID()}><span className={`${username===message.user? 'text-blue-400' : 'text-red-400'}`}>{message.user}</span> : {message.message}</li>)
+          }
+       </ul>
+      </div>
+            
+      
+      
+        <form action="" className="flex gap-2 w-full justify-center max-sm:flex-col"  onSubmit={onSubmitMessage}>
+        
+          <input
+            placeholder="Message..."
+            className="bg-gray-200 dark:bg-gray-700 rounded-full py-2 px-6 w-[80%] border-2 border-gray-500 max-[820px]:w-[60%] max-sm:w-full"            
+            ref={textInput}           
+          />
+          <button type="submit" className="bg-blue-700 hover:bg-blue-950 text-white font-bold py-2 px-4 rounded-full w-[20%] max-[820px]:w-[40%] max-sm:w-full">Send Message</button>
+      </form>
     </div>
   );
 }
